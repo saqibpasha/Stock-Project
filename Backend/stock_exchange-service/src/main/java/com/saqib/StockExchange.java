574a8fa19a4c7c4a43b0a1cfc08fe58e892a89d1
saqibpasha <saqibpasha6@gmail.com>
2020-08-01 22:06:45 +0530

    Update StockExchange.java

--- Backend/stock_exchange-service/src/main/java/com/saqib/StockExchange.java
+++ Backend/stock_exchange-service/src/main/java/com/saqib/StockExchange.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
+@SequenceGenerator(name="seq", initialValue=6, allocationSize=1000)
 public class StockExchange 
 {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="seq")
 	int id;
 	String name, brief, address, remarks;
 	public int getId() {
